27a31b16f93a12a340de546795aa7b73e5e34903
Nartov Dima <nartov.dima2018@yandex.ru>
2023-05-18 19:26:15 +0300

    Reduced memory usage 5

--- libraries/acp/include/acp/Pool.hpp
+++ libraries/acp/include/acp/Pool.hpp
@@ -18,13 +18,10 @@ public:
 private:
     static constexpr std::size_t npos = static_cast<std::size_t>(-1);
 
-    std::size_t block_size;
     std::vector<std::size_t> obj_sizes;
 
     std::vector<std::vector<std::byte>> m_storage;
     std::vector<std::vector<bool>> m_used_map;
-    std::unordered_map<std::size_t, std::size_t> m_obj_sizes_map;
-    std::unordered_map<const std::byte*, std::size_t> m_sizes_map;
 
     std::size_t find_empty_place(std::size_t const n, std::size_t const block_ind);
 };
--- libraries/acp/src/Pool.cpp
+++ libraries/acp/src/Pool.cpp
@@ -1,16 +1,13 @@
 #include "acp/Pool.hpp"
 
+#include <algorithm>
 #include <cassert>
 
 PoolAllocator::PoolAllocator(std::size_t const block_size, std::initializer_list<std::size_t> sizes)
-    : block_size(block_size)
-    , obj_sizes(sizes)
-    , m_storage(sizes.size(), std::vector<std::byte>(block_size))
-    , m_used_map(sizes.size()) {
+    : obj_sizes(sizes), m_storage(sizes.size(), std::vector<std::byte>(block_size)), m_used_map(sizes.size()) {
     std::size_t block_ind = 0;
     for (const auto& it : sizes) {
         m_used_map[block_ind].resize(block_size / it);
-        m_obj_sizes_map[it] = block_ind;
         block_ind++;
     }
 }
@@ -25,21 +22,26 @@ std::size_t PoolAllocator::find_empty_place(std::size_t const n, std::size_t con
 }
 
 void* PoolAllocator::allocate(std::size_t const n) {
-    std::size_t block_ind = m_obj_sizes_map[n];
+    std::size_t block_ind = std::distance(obj_sizes.begin(), std::find(obj_sizes.begin(), obj_sizes.end(), n));
     const std::size_t pos = find_empty_place(n, block_ind);
     if (pos != npos) {
         m_used_map[block_ind][pos] = true;
         const auto ptr             = &m_storage[block_ind][pos * n];
-        m_sizes_map[ptr]           = n;
         return ptr;
     }
     throw std::bad_alloc{};
 }
 
 void PoolAllocator::deallocate(void const* ptr) {
-    auto b_ptr            = static_cast<const std::byte*>(ptr);
-    std::size_t block_ind = m_obj_sizes_map[m_sizes_map[b_ptr]];
-
+    auto b_ptr = static_cast<const std::byte*>(ptr);
+    std::size_t block_ind =
+        std::distance(m_storage.begin(),
+                      std::find_if(m_storage.begin(), m_storage.end(), [&b_ptr](const std::vector<std::byte>& block) {
+                          return (block.data() <= b_ptr && b_ptr < block.data() + block.size());
+                      }));
+    if (block_ind == m_storage.size()) {
+        return;
+    }
     const auto begin         = &m_storage[block_ind][0];
     const std::size_t offset = (b_ptr - begin) / obj_sizes[block_ind];
     assert((b_ptr - begin) % obj_sizes[block_ind] == 0);
